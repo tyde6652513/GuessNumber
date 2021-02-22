02ba6600ed9bedccfe0e9a1a8cd24e6f79be5f62
Darcy <tyde6652513@gmail.com>
2021-02-22 12:12:00 +0800

    猜數字App
    初版 無視窗控制

---
+++ GuessNumber/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DefineNumber;
+
+namespace GuessNumber
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+
+    public partial class MainWindow : Window
+    {
+        int GuessTime = 0;
+        private Number NewNumber = new Number();
+        public delegate void D();
+        public D HideDelegate;
+        //AnsWindow Form;
+        public MainWindow()
+        {
+
+            NewNumber.CreateAnswer();
+            InitializeComponent();
+            //Form = new AnsWindow();
+            //this.Ans.Click += new System.Windows.RoutedEventHandler(this.Ans_Click);
+            //Form.Closed +=  new System.EventHandler(this.Show);
+        }
+
+
+
+        private void Ans_Click(object sender, RoutedEventArgs e)
+        {
+    
+            MessageBox.Show("答案是:\n  " + NewNumber.RightAnswer);
+            //Form.Show();
+            //Form.DisplayAnswer.Text = "  答案是:\n  " + NewNumber.RightAnswer;
+            //Form.Closed += 
+            //this.Hide();
+
+
+            //this.Close();
+            ///this.HideDelegate = new D(this.Hide);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            NewNumber.UserAnswer = AnsBox.Text;
+            if (NewNumber.CheckFormat())
+            {
+                GuessTime++;
+                Display.Items.Add(AnsBox.Text + "-->第" + GuessTime + "次猜測:  " + NewNumber.GetA() + "A" + NewNumber.GetB() + "B");
+                if (NewNumber.GetA() == 4)
+                {
+                    MessageBox.Show("Bingo 答對了!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("輸入格式錯誤", "Error");
+            }
+
+
+        }
+
+        private void NewGame_Click(object sender, RoutedEventArgs e)
+        {
+            NewNumber.CreateAnswer();
+            AnsBox.Clear();
+            Display.Items.Clear();            
+        }
+        
+    }
+}
---
+++ GuessNumber/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+namespace DefineNumber
+{
+    class Number
+    {
+        private string _RightAnswer,_UserAnswer;
+        
+        public string RightAnswer
+        {
+            get{return _RightAnswer;}
+            set{
+                _RightAnswer = value;
+            }
+        }
+        public string UserAnswer
+        {
+            get{return _UserAnswer;}
+            set{_UserAnswer = value;}
+        }
+        public void CreateAnswer()
+        {
+            int[] temp = new int[4];
+            Random R = new Random();
+            for (int j = 0;j<4;j++)
+            {
+                if (j == 0){
+                    temp[j] = R.Next(0,10);
+                }
+                else{
+                    temp[j] = R.Next(0,10);
+                    for (int k = 0;k<j;k++)
+                    {
+                        if (temp[k] == temp[j]){
+                            j--;
+                            break;
+                        }
+                    }
+                }         
+            }
+            RightAnswer = string.Concat(temp);
+        }
+        public int GetA()
+        {
+            int FlagA = 0;
+            for (int i = 0;i<UserAnswer.Length;i++){
+                if(UserAnswer[i] == RightAnswer[i]){
+                    FlagA++;
+                }
+            }
+            return FlagA;
+        }
+        public int GetB()
+        {
+            int FlagB = 0;
+            for (int i = 0;i<RightAnswer.Length;i++){
+                for(int j = 0;j<RightAnswer.Length;j++){
+                    if ((i != j) && (UserAnswer[i] == RightAnswer[j])){
+                        FlagB++;
+                        break;
+                    }
+                }
+            }
+            return FlagB;
+        }
+        public bool CheckFormat() 
+        {
+            bool Bflag = true;
+            int SameFlag;
+            if (UserAnswer.Length == 4)
+            {               
+                for (int i = 0; i < UserAnswer.Length; i++)
+                {
+                    if ((Convert.ToInt16(UserAnswer[i]) > 57) | (Convert.ToInt16(UserAnswer[i]) < 48))
+                    {
+                        Bflag = false;
+                    }
+                    else
+                    {
+                        SameFlag = 0;
+                        for (int j = 0; j < UserAnswer.Length; j++)
+                        {
+                            if (UserAnswer[i] == UserAnswer[j])
+                            {
+                                SameFlag++;
+                            }
+                            if (SameFlag > 1)
+                            {
+                                Bflag = false;
+                                break;
+                            }
+                        }
+                    }
+                    if (!Bflag) break;
+                }
+                return Bflag;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
